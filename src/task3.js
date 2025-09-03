b2285193809a46985c2dc94535ecd288295a98d0
Arturo <monster.304@outlook.es>
2025-09-03 16:17:25 -0600

    Agrego Laboratorio de Arrays Arturo Ramirez

--- src/task3.js
+++ src/task3.js
@@ -1,8 +1,8 @@
 const arr = [1,2,3,4,5,6,7,9,10,11,12]
 
 // Escribe tu codigo por debajo de esta linea
-
-
+// task 3: Deberá tener el numero 8 en el indice 7 y un length de 12
+arr.splice(7, 0, 8);
 
 // TEscribe tu codigo por encima de esta linea
 

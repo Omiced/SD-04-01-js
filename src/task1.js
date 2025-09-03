047977c9164537a2fecba94a76469dc0a799f651
Raul Grant <vqrgashernandezrauldejesus@gmail.com>
2025-09-03 16:56:32 -0600

    Cambios Raúl De Jesús Vargas Hernández

--- src/task1.js
+++ src/task1.js
@@ -2,9 +2,14 @@ const arr = []
 
 // Escribe tu codigo por debajo de esta linea
 
+for (let i = 1; i <= 20; i++) {
+// intento 1
+  arr.push(i);
+}
+
+console.log(arr);
 
 
-// Escribe tu codigo por encima de esta linea
 
 //No tocar esta funcion
 export function taskArr(){
